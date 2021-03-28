d3bac7ddc06bf9f515d675fce7861a2ad731123c
lorelles <74384017+lorelles@users.noreply.github.com>
2021-03-27 19:24:24 -0700

    create script

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,4 +1,37 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
 
 // YOUR CODE HERE!
+//let button = d3.select("#button"),
+    //form = d3.select("#form");
+
+// Get a reference to the table body
+// const tbody = d3.select("tbody");
+
+// Complete the event handler function for the form
+//const runEnter = () => {
+    
+    // Prevent the page from refreshing
+    //d3.event.preventDefault();
+
+    // Loop Through `data` and console.log each UFO report object
+    //data.forEach(sightings => console.log(sightings));
+
+// // Use d3 to update each cell's text with UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
+//     sightings.forEach(sightings => {
+//     let row = tbody.append("tr");
+//     Object.values(sightings).forEach(value => {
+//     // Append a cell to the row for each value in the UFO report object
+//     var cell = row.append("td");
+//     cell.text(value);
+//     });
+// });
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    //let inputElement = d3.select("#datetime"),
+    //inputValue = inputElement.property("value");
+
+//console.log(inputValue);
+
+
+//};
